1f4919b6d1432d0a7348f0e599c8f6790afed93a
André Pinheiro <andrepdpinheiro@gmail.com>
2017-03-04 16:45:28 +0000

    Initial setup

--- src/github.com/pluralism/clagocerqueira/server/server.go
+++ src/github.com/pluralism/clagocerqueira/server/server.go
@@ -4,6 +4,7 @@ import (
 	iris "gopkg.in/kataras/iris.v6"
 	"gopkg.in/kataras/iris.v6/adaptors/cors"
 	"gopkg.in/kataras/iris.v6/adaptors/httprouter"
+	"gopkg.in/kataras/iris.v6/adaptors/view"
 )
 
 func main() {
@@ -14,10 +15,18 @@ func main() {
 		iris.DevLogger(),
 		// Router from adaptors/httprouter
 		httprouter.New(),
+		// HTML standard engine for all files inside "../client/app/views" folder with extension ".html"
+		view.HTML("../client/app/views", ".html"),
 		// Allows all origins
 		cors.New(cors.Options{AllowedOrigins: []string{"*"}}))
 
-	app.Get("/", func(context *iris.Context) {
+	app.Get("/", homePageHandler)
 
-	})
+	// Start the server on port 8080
+	app.Listen(":8080")
+}
+
+func homePageHandler(context *iris.Context) {
+	// Enable gzip for better compression
+	context.Render("index.html", iris.RenderOptions{"gzip": true})
 }
