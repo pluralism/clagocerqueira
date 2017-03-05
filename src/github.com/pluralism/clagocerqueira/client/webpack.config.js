56867009cb5d76ea20ac3c55dda756669540ae7d
André Pinheiro <andrepdpinheiro@gmail.com>
2017-03-05 11:19:02 +0000

    Removed prod folder

--- src/github.com/pluralism/clagocerqueira/client/webpack.config.js
+++ src/github.com/pluralism/clagocerqueira/client/webpack.config.js
@@ -1,5 +1,6 @@
 let path = require('path');
 let webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 
 function join(dest) {
@@ -12,22 +13,35 @@ function web(dest) {
 }
 
 
+const extractText = new ExtractTextPlugin({
+  filename: "[name].css"
+});
+
+
 module.exports = {
   entry: {
     application: [
-      web('application.js')
+      web('application.js'),
+      web('static/theme/app.css'),
+      web('static/theme/style-one.css'),
+      web('static/font-awesome/css/font-awesome.min.css'),
+      //web('static/cube-portfolio/cubeportfolio/css/cubeportfolio.min.css'),
+      //web('static/cube-portfolio/cubeportfolio/custom/custom-cubeportfolio.css'),
+      //web('static/owl-carousel2/assets/owl.carousel.css'),
+      //web('static/line-icons/line-icons.css'),
+      web('static/theme/style.scss'),
+      web('static/theme/globals.css'),
     ]
   },
 
   output: {
     path: join('app/static/prod'),
-    filename: 'application.js',
-    publicPath: ''
+    filename: "[name].js"
   },
 
 
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -36,6 +50,53 @@ module.exports = {
           cacheDirectory: true,
           presets: ['env', 'react']
         }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loader: extractText.extract({
+          use: [{
+            loader: "css-loader"
+          }, {
+            loader: "sass-loader"
+          }]
+        })
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader: extractText.extract({
+          use: [{
+            loader: "css-loader"
+          }]
+        })
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        exclude: /node_modules/,
+        loaders: ['file-loader?hash=sha512&digest=hex&name=images/[name]_[hash].[ext]', {
+          loader: 'image-webpack-loader',
+          query: {
+            mozjpeg: {
+              progressive: true,
+            },
+            gifsicle: {
+              interlaced: false,
+            },
+            optipng: {
+              optimizationLevel: 7,
+            },
+            pngquant: {
+              quality: '75-90',
+              speed: 3,
+            },
+          },
+        }]
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        exclude: /node_modules/,
+        loader: 'file-loader?hash=sha512&digest=hex&name=fonts/[name]_[hash].[ext]'
       }
     ]
   },
@@ -50,6 +111,7 @@ module.exports = {
     new webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery"
-    })
+    }),
+    extractText
   ]
 };
