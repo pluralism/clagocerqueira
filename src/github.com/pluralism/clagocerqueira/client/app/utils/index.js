b62105cc11b3fe1171f3405bfaa2732517de2386
André Pinheiro <andrepdpinheiro@gmail.com>
2017-03-14 20:33:41 +0000

    Creating utils function

---
+++ src/github.com/pluralism/clagocerqueira/client/app/utils/index.js
@@ -0,0 +1,41 @@
+const defaultHeaders = {
+  /**
+   * Content-Type header: Indicates the media type of the entity-body
+   * sent to the recipient
+  */
+  'Content-Type': 'application/graphql'
+};
+
+
+function buildHeaders() {
+  return { ...defaultHeaders };
+}
+
+
+export function parseJSON(response) {
+  return response.json();
+}
+
+
+export function checkStatus(response) {
+  if(response.status >= 200 && response < 300)
+    return response;
+  else{
+    let error = new Error(response.statusText);
+    error.response = response;
+    throw error;
+  }
+}
+
+
+export function httpPost(url, data) {
+  const body = JSON.stringify(data);
+
+  return fetch(url, {
+    method: 'POST',
+    headers: buildHeaders(),
+    body: body
+  })
+  .then(checkStatus)
+  .then(parseJSON);
+}
