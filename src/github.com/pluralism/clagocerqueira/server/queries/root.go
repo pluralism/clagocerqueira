09ba060902e820f254151605b36d38691ab62083
André Pinheiro <andrepdpinheiro@gmail.com>
2017-03-18 11:32:45 +0000

    Creating new queries

--- src/github.com/pluralism/clagocerqueira/server/queries/root.go
+++ src/github.com/pluralism/clagocerqueira/server/queries/root.go
@@ -1,8 +1,29 @@
 package queries
 
-import "github.com/graphql-go/graphql"
+import (
+	"errors"
+
+	"github.com/graphql-go/graphql"
+)
 
 var RootQuery = graphql.NewObject(graphql.ObjectConfig{
-	Name:   "CLagoCerqueiraRootQuery",
-	Fields: graphql.Fields{},
+	Name: "CLagoCerqueiraRootQuery",
+	Fields: graphql.Fields{
+		"presidents": &graphql.Field{
+			Description: "Extract objects in a given date",
+			Args: graphql.FieldConfigArgument{
+				"date": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+			},
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				_, ok := p.Args["date"].(string)
+
+				if !ok {
+					return nil, errors.New("the date does not exist")
+				}
+				return nil, nil
+			},
+		},
+	},
 })
