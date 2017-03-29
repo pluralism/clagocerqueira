9445fe9daea413b523c4b2a4a017eea9a283af64
André Pinheiro <andrepdpinheiro@gmail.com>
2017-03-29 21:29:32 +0100

    Creating personalities front-end

---
+++ src/github.com/pluralism/clagocerqueira/client/app/views/personalities/index.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+import Footer from '../../components/common/footer';
+import classNames from 'classnames';
+import Constants from '../../constants/index';
+
+
+class PersonalitiesView extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      canSwitchPage: true
+    };
+  }
+
+
+  componentDidMount() {
+    const { dispatch } = this.props;
+  }
+
+
+  renderHeader() {
+    return (
+      <nav className="clc-header one-page-header navbar navbar-default navbar-fixed-top navbar-toggleable-sm" data-role="navigation">
+        <div className="navbar-header">
+          <div className="menu-container page-scroll">
+            <button type="button" className="navbar-toggle" data-toggle="collapse"
+              data-target=".clc-collapse">
+              <span className="sr-only">Toggle Navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+
+            <a href="#body" className="navbar-brand main-font">
+              <img src={require('../../static/img/logo.jpg')} alt="Logo" className="img-responsive" />
+            </a>
+          </div>
+        </div>
+
+        <div className="collapse navbar-collapse clc-collapse">
+          <ul className="nav navbar-nav">
+            <li className="page-scroll home">
+              <a href="#">Início</a>
+            </li>
+
+            <li className="page-scroll">
+              <a href="#autores_amarantinos">Autores Amarantinos</a>
+            </li>
+
+            <li className="page-scroll">
+              <a href="#orgaos_autarquicos">Órgãos Autárquicos</a>
+            </li>
+
+            <li className="page-scroll">
+              <a href="#associacoes">Associações</a>
+            </li>
+
+            <li className="page-scroll">
+              <a href="#festividades">Festividades</a>
+            </li>
+
+            <li className="page-scroll">
+              <a href="#imprensa">Imprensa</a>
+            </li>
+
+            <li className="page-scroll">
+              <a href="#personalidades">Personalidades</a>
+            </li>
+
+            <li className="page-scroll">
+              <a href="#contacto">Contacto</a>
+            </li>
+
+            <li className="page-scroll">
+              <a href="#patrimonio_natural">Património Natural</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+
+
+  render() {
+    return ();
+  }
+}
