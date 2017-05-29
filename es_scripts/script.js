4d998dd31cf85f12f46a5ffee6e79231edc70f67
André Pinheiro <andrepdpinheiro@gmail.com>
2017-05-29 16:03:29 +0100

    Adding the scripts now

---
+++ es_scripts/script.js
@@ -0,0 +1,49 @@
+let fs = require('fs');
+let request = require('request');
+let es = require('elasticsearch');
+
+let client = new es.Client({
+  host: 'localhost:9200'
+});
+
+
+function callback(error, response) {
+    if (error) {
+        console.log(error);
+    }
+}
+
+function indexData(filename, indexname, type) {
+  fs.readFile(filename, {encoding: 'utf-8'}, function(err, data) {
+    data = JSON.parse(data);
+    let bulk_request = [];
+    for(var i = 0; i < data.length; i++) {
+      let obj = data[i];
+      let objFinal = {
+        id: obj._id.$oid,
+        name: obj.name,
+        objects: obj.objects
+      };
+      bulk_request.push({index: {_index: indexname, _type: type, _id: objFinal.id}});
+      bulk_request.push(objFinal);
+    }
+
+    let insertData = function() {
+      client.bulk({
+        body: bulk_request.slice(0, 1000)
+      }, callback);
+    }
+
+    bulk_request = bulk_request.slice(0, 1000);
+    console.log(bulk_request.length);
+
+    if(bulk_request.length > 0) {
+      setTimeout(insertData, 1000);
+    } else {
+      console.log('Inserted all records!')
+    }
+  });
+}
+
+
+indexData('./parishes_presidents.json', 'parishes_presidents', 'parishes_president');
