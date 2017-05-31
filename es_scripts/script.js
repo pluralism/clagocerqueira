e20986a760a429dd35769b2495a0b42ec6332619
André Pinheiro <andrepdpinheiro@gmail.com>
2017-05-31 01:41:58 +0100

    Replaced some es_scripts json

--- es_scripts/script.js
+++ es_scripts/script.js
@@ -46,4 +46,43 @@ function indexData(filename, indexname, type) {
 }
 
 
-indexData('./parishes_presidents.json', 'parishes_presidents', 'parishes_president');
+
+function indexDataParishesPresidents(filename, indexname, type) {
+  fs.readFile(filename, {encoding: 'utf-8'}, function(err, data) {
+    data = JSON.parse(data);
+    let bulk_request = [];
+    for(var i = 0; i < data.length; i++) {
+      let obj = data[i];
+      let objFinal = {
+        id: obj._id.$oid,
+        name: obj.name,
+        objects: obj.dates.objects
+      };
+      // Add an extra field for parishes presidents
+      objFinal.objects.objects_data.extra = obj.dates.name;
+
+      bulk_request.push({index: {_index: indexname, _type: type, _id: objFinal.id}});
+      bulk_request.push(objFinal);
+    }
+
+    let insertData = function() {
+      client.bulk({
+        body: bulk_request.slice(0, 1000)
+      }, callback);
+    }
+
+    bulk_request = bulk_request.slice(0, 1000);
+    console.log(bulk_request.length);
+
+    if(bulk_request.length > 0) {
+      setTimeout(insertData, 1000);
+    } else {
+      console.log('Inserted all records!')
+    }
+  });
+}
+
+
+
+
+indexData('./press.json', 'press', 'press');
