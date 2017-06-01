ff87efac692afd63b8ad1730be1f97aee9906e57
André Pinheiro <andrepdpinheiro@gmail.com>
2017-06-01 22:36:04 +0100

    Everything seems to be ready for production

--- src/github.com/pluralism/clagocerqueira/client/app/views/search/index.js
+++ src/github.com/pluralism/clagocerqueira/client/app/views/search/index.js
@@ -62,8 +62,7 @@ class SearchView extends React.Component {
                             <h2><span className="g-color-default">Pesquisa</span></h2>
                         </div>
                         <p className="g-page-title">
-                            Nam sed erat aliquet libero aliquet commodo.
-                            Donec euismod augue non quam finibus, nec iaculis tellus gravida. Integer <br /> efficitur eros ut dui laoreet, ut blandit turpis tincidunt.
+                            Resultados de pesquisa para <strong>{this.props.location.query.value}</strong>
                         </p>
                     </div>
 
