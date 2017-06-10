7ac1649983d770624a606289da969dd6669c1ce2
André Pinheiro <andrepdpinheiro@gmail.com>
2017-06-10 11:32:04 +0100

    Create script.js

--- es_scripts/script.js
+++ es_scripts/script.js
@@ -48,8 +48,6 @@ function indexData(filename, indexname, type) {
 }
 
 
-
-
 function indexDataParishesPresidents(filename, indexname, type) {
   fs.readFile(filename, {encoding: 'utf-8'}, function(err, data) {
     data = JSON.parse(data);
