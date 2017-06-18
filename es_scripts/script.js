8ab8798c78778cde1b5e0e2452e37e6fd065e920
André Pinheiro <andrepdpinheiro@gmail.com>
2017-06-18 20:04:08 +0100

    Update script.js

--- es_scripts/script.js
+++ es_scripts/script.js
@@ -1,15 +1,15 @@
 let fs = require('fs');
 let request = require('request');
 let es = require('elasticsearch');
-let client = new es.Client({ host: 'localhost:9200' });
-
+let client = new es.Client({ 
+    host: 'localhost:9200' 
+});
 
 function callback(error, response) {
     if (error) {
         console.log(error);
     }
 }
-
 
 
 function indexData(filename, indexname, type) {
