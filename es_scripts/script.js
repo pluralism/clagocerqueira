d751451b505f772781bd6716f7a89c726fd762d7
André Pinheiro <andrepdpinheiro@gmail.com>
2017-06-24 02:16:05 +0100

    Update script.js

--- es_scripts/script.js
+++ es_scripts/script.js
@@ -28,7 +28,6 @@ function indexData(filename, indexname, type) {
       bulk_request.push({index: {_index: indexname, _type: type, _id: objFinal.id}});
       bulk_request.push(objFinal);
     }
-    
 
     let insertData = function() {
       client.bulk({
