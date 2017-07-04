cc426db15ef9b8f9515eae3e84dca67ebd81222c
André Pinheiro <andrepdpinheiro@gmail.com>
2017-07-04 21:01:33 +0100

    Update script.js

--- es_scripts/script.js
+++ es_scripts/script.js
@@ -3,13 +3,11 @@ let request = require('request');
 let es = require('elasticsearch');
 let client = new es.Client({ host: 'localhost:9200' });
 
-
 function callback(error, response) {
     if (error)
         console.log(error);
 }
  
-
 function indexData(filename, indexname, type) {
   fs.readFile(filename, {encoding: 'utf-8'}, function(err, data) {
     data = JSON.parse(data);
