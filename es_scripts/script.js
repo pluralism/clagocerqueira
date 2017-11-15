999163da58b3ec89e3956a213f5053b369498d24
André Pinheiro <andrepdpinheiro@gmail.com>
2017-11-15 15:27:25 +0000

    Update script.js

--- es_scripts/script.js
+++ es_scripts/script.js
@@ -63,12 +63,14 @@ function indexDataParishesPresidents(filename, indexname, type) {
         };
         
         bulk_request = bulk_request.slice(0, 1000);
-        if(bulk_request.length > 0)
+        if(bulk_request.length > 0) {
             setTimeout(insertData, 1000);
-        else
+        } else {
             console.log('Inserted all records!');
+        }
     });
 }
+
 
 // indexData('./associations.json', 'associations', 'association');
 // indexData('./festivities.json', 'festivites', 'festivity');
