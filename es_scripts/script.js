9dc9cea72e7abbd3b812b3f80ae5c07e8f65f840
André Pinheiro <andrepdpinheiro@gmail.com>
2017-12-30 13:56:40 +0000

    Update script.js

--- es_scripts/script.js
+++ es_scripts/script.js
@@ -1,9 +1,7 @@
 let fs = require('fs');
 let es = require('elasticsearch');
 
-let client = new es.Client({ 
-    host: 'localhost:9200'
-});
+let client = new es.Client({ host: 'localhost:9200' });
 
 function insertCallback(error, response) {
     if (error) {
