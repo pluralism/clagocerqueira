c3e9e36d8bf503ecaba8401f747ec5ffbd6ae21a
André Pinheiro <andrepdpinheiro@gmail.com>
2018-02-12 21:47:49 +0000

    Update script.js

--- es_scripts/script.js
+++ es_scripts/script.js
@@ -35,7 +35,6 @@ function indexData(filename, indexname, type) {
         };
         
         bulk_request = bulk_request.slice(0, 1000);
-        
         if(bulk_request.length > 0) {
             setTimeout(insertData, 1000);
         } else {
