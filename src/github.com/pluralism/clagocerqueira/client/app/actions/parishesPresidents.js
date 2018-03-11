7218ad9c02936e27b046f626d2bc97273e29c604
André Pinheiro <andrepdpinheiro@gmail.com>
2018-03-11 23:14:34 +0000

    Update parishesPresidents.js

--- src/github.com/pluralism/clagocerqueira/client/app/actions/parishesPresidents.js
+++ src/github.com/pluralism/clagocerqueira/client/app/actions/parishesPresidents.js
@@ -58,6 +58,68 @@ ParishesPresidentsActions.getParishPresidentsByPage = (date, name, mapping, page
     );
 };
 
+ParishesPresidentsActions.getDatesForParish = (parishName, dateAndPageMappings, state, generalMappings) => {
+    return dispatch => {
+        dispatch({
+            type: Constants.LOADING_DATA_PARISHES_PRESIDENTS
+        });
+
+        const graphQLData = `{
+            parish_dates: parishes_presidents_dates(name: "${parishName}") {
+                name
+                dates {
+                    name
+                }
+            }
+        }`;
+    
+        httpPostGraphQL(graphQLData)
+            .then((data) => {
+                // Something went wrong...
+                if(data.hasOwnProperty('errors')) {
+                    dispatch({
+                        type: Constants.LOADING_DATA_ERROR_PARISHES_DATES
+                    });
+                } else {
+                    // Success, extract the data to the user
+                    var parishesDates = data['data']['parish_dates'];
+                    var dates = [];
+                    generalMappings = [];
+                    dateAndPageMappings = {};
+
+                    for(var i = 0; i < parishesDates['dates'].length; i++) {
+                        let date = parishesDates['dates'][i]['name'];
+                        dates.push(date);
+                        dateAndPageMappings[date] = {
+                            mapping: "data".concat(date.replace("-", "_")),
+                            page: 1
+                        };
+                        generalMappings.push(["data".concat(date.replace("-", "_")), date]);
+                    }
+
+                    dates.sort(function(a, b) {
+                        return parseInt(a.split('-')[1]) - parseInt(b.split('-')[1]);
+                    });
+
+                    state.activeTab = dates[0];
+
+                    dispatch({
+                        type: Constants.LOADING_DATA_SUCCESS_PARISHES_DATES,
+                        data: dates,
+                        generalMappings: generalMappings,
+                        dateAndPageMappings: dateAndPageMappings,
+                        currentDate: dates[0]
+                    });
+
+                    dispatch(ParishesPresidentsActions.getAllDataFromParishesPresidents(
+                        parishName,
+                        generalMappings
+                    ));
+                }
+            });
+    };
+};
+
 
 ParishesPresidentsActions.getDataByPage =
     (name, date, mapping, page, type, loadingAction, errorAction, successAction) => {
@@ -105,8 +167,6 @@ ParishesPresidentsActions.getDataByPage =
     };
 };
 
-
-
 ParishesPresidentsActions.getAllDataFromParishesPresidents = (name, mappings) => {
     return ParishesPresidentsActions.loadDataFromServer(
         name,
@@ -116,8 +176,6 @@ ParishesPresidentsActions.getAllDataFromParishesPresidents = (name, mappings) =>
         1,
         mappings);
 };
-
-
 
 ParishesPresidentsActions.loadDataFromServer =
     (name, loadingAction, errorAction, successAction, page, mappings) => {
